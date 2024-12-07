229dd474bee976e8eb9fa80f16420a636fd13221
Spidy394 <mondalshubhodeep30924@gmail.com>
2024-12-07 20:26:49 +0530

    add Pop-Up

---
+++ Pop-up/script.js
@@ -0,0 +1,9 @@
+let popUp = document.getElementById("pop-up");
+
+function openPopUp(){
+    popUp.classList.add("openPopUp")
+}
+
+function closePopUp(){
+    popUp.classList.remove("openPopUp")
+}
