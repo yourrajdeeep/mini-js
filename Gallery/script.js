596b8e1a31754e2184a2c55e3bc153149dd0f35f
Spidy394 <mondalshubhodeep30924@gmail.com>
2024-12-09 12:59:02 +0530

    Add Gallery

---
+++ Gallery/script.js
@@ -0,0 +1,20 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("back-btn");
+let nextBtn = document.getElementById("next-btn");
+
+
+scrollContainer.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    scrollContainer.scrollLeft += e.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += -900;
+});
